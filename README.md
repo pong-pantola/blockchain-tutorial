62e85dbe4de29048d388af70ccdfc82ded90adf9
Alexis V. Pantola <pantolav@ph.ibm.com>
2019-07-18 16:32:27 +0800

    changed to relative path

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # blockchain-tutorial
 
+https://github.com/pong-pantola/blockchain-tutorial.git
+
+curl -sSL http://bit.ly/2ysbOFE | bash -s -- 1.4.0 1.4.0 0.4.15
+
 curl -sSL http://bit.ly/2ysbOFE | bash -s -- 1.4.1 1.4.1 0.4.15
 
 update:
@@ -12,3 +16,20 @@ fabric-samples/basic-network/docker-compose.yml
   
 blockchain-tutorial/blue-coin/loadBlueCoin.js
   -update FABRIC_SAMPLES_HOME=/home/osboxes/testing/fabric-samples
+
+
+
+mkdir training
+cd training
+curl -sSL http://bit.ly/2ysbOFE | bash -s -- 1.4.0 1.4.0 0.4.15
+git clone https://github.com/pong-pantola/blockchain-tutorial.git
+
+open fabric-samples/basic-network/docker-compose.yml
+  -look for the following:
+
+    volumes:
+        - ./../chaincode/:/opt/gopath/src/github.com/
+
+  -change to
+        #- ./../chaincode/:/opt/gopath/src/github.com/
+        - ./../../blockchain-tutorial/chaincode/:/opt/gopath/src/github.com/
--- chaincode/loadBlueCoin.sh
+++ chaincode/loadBlueCoin.sh
@@ -7,15 +7,21 @@
 # Exit on first error
 set -e
 
-FABRIC_SAMPLES_HOME=/home/osboxes/testing/fabric-samples
+#FABRIC_SAMPLES_HOME=/home/osboxes/testing/fabric-samples
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
 
 CC_RUNTIME_LANGUAGE=node # chaincode runtime language is node.js
-CC_SRC_PATH=/opt/gopath/src/github.com/blue-coin
+CC_SRC_PATH=/opt/gopath/src/github.com/fabcar
 
+#stop docker containers
+docker stop $(docker ps -aq)
+docker rm -f $(docker ps -aq)
+
+#delete all images with the name dev-peer
+docker rmi -f $(docker images | grep dev-peer | awk '{print $3}')
 
 # clean the keystore
 #rm -rf ./hfc-key-store
@@ -24,22 +30,17 @@ mkdir ../wallet
 
 
 # launch network; create channel and join peer to channel
-cd $FABRIC_SAMPLES_HOME/basic-network
+cd ../../fabric-samples/basic-network
 ./start.sh
 
 # Now launch the CLI container in order to install, instantiate chaincode
-# and prime the ledger with our 10 cars
 docker-compose -f ./docker-compose.yml up -d cli
-docker ps -a
 
-docker exec \
-  -e "CORE_PEER_LOCALMSPID=Org1MSP" \
-  -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n bluecoin -v 1.0 -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
-docker exec \
-  -e "CORE_PEER_LOCALMSPID=Org1MSP" \
-  -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n bluecoin -l "$CC_RUNTIME_LANGUAGE" -v 1.0 -c '{"Args":[]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n fabcar -v 1.0 -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n fabcar -l "$CC_RUNTIME_LANGUAGE" -v 1.0 -c '{"Args":[]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 sleep 10
-docker exec \
-  -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n bluecoin -c '{"function":"initLedger","Args":[]}'
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"function":"initLedger","Args":[]}'
 
 echo Loading of chaincode blue-coin is complete.
+
+
