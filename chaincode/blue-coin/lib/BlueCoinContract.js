1a2a288668b809550c7ddff2069808be45d567ce
Alexis V. Pantola <pantolav@ph.ibm.com>
2019-07-23 10:16:33 +0800

    moved query function to Utility

--- chaincode/blue-coin/lib/BlueCoinContract.js
+++ chaincode/blue-coin/lib/BlueCoinContract.js
@@ -89,18 +89,7 @@ class BlueCoinContract extends Contract {
     return shim.success({"status" :"success","message":"Getting records above " + val + " blue coin","result": queryResult });
   }
 
-  async getQueryResult(ctx, jsonQuery) {
-    console.info('============= START : GET QUERY RESULT =============');
-    console.info("jsonQuery: " + JSON.stringify(jsonQuery, null, 4));
 
-    let strQuery = JSON.stringify(jsonQuery);
-    let resultIterator = await ctx.stub.getQueryResult(strQuery);
-
-    let resultArr = await this.iteratorToArrayResult(resultIterator, false);
-
-    console.info('============= END : GET QUERY RESULT =============');
-    return resultArr;
-  }
 
   async getHistoryForMarble(stub, args, thisClass) {
 
@@ -117,45 +106,6 @@ class BlueCoinContract extends Contract {
     return Buffer.from(JSON.stringify(results));
   }
 
-  async iteratorToArrayResult(iterator, isHistory) {
-    console.info('============= START : Iterator to Array Result =============');
-    let arr = [];
-    while (true) {
-      let res = await iterator.next();
-
-      if (res.value && res.value.value.toString()) {
-        let jsonRes = {};
-        //console.log(res.value.value.toString('utf8'));
-
-        if (isHistory && isHistory === true) {
-          jsonRes.TxId = res.value.tx_id;
-          jsonRes.Timestamp = res.value.timestamp;
-          jsonRes.IsDelete = res.value.is_delete.toString();
-          try {
-            jsonRes.Value = JSON.parse(res.value.value.toString('utf8'));
-          } catch (err) {
-            console.log(err);
-            jsonRes.Value = res.value.value.toString('utf8');
-          }
-        } else {
-          jsonRes.Key = res.value.key;
-          try {
-            jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
-          } catch (err) {
-            console.log(err);
-            jsonRes.Record = res.value.value.toString('utf8');
-          }
-        }
-        arr.push(jsonRes);
-      }
-      if (res.done) {
-        await iterator.close();
-        console.info("arr: "+JSON.stringify(arr, null, 4));
-        console.info('============= END : Iterator to Array Result =============');
-        return arr;
-      }
-    }
-  }
 }
 
 module.exports = BlueCoinContract;
--- chaincode/blue-coin/lib/Utility.js
+++ chaincode/blue-coin/lib/Utility.js
@@ -24,6 +24,58 @@ class Utility{
     await ctx.stub.putState(key, this.jsonToBytes(json));
   }
 
+  static async getQueryResult(ctx, jsonQuery) {
+    console.info('============= START : GET QUERY RESULT =============');
+    console.info("jsonQuery: " + JSON.stringify(jsonQuery, null, 4));
+
+    let strQuery = JSON.stringify(jsonQuery);
+    let resultIterator = await ctx.stub.getQueryResult(strQuery);
+
+    let resultArr = await Utility.iteratorToArrayResult(resultIterator, false);
+
+    console.info('============= END : GET QUERY RESULT =============');
+    return resultArr;
+  }
+  
+  static async iteratorToArrayResult(iterator, isHistory) {
+    console.info('============= START : Iterator to Array Result =============');
+    let arr = [];
+    while (true) {
+      let res = await iterator.next();
+
+      if (res.value && res.value.value.toString()) {
+        let jsonRes = {};
+        //console.log(res.value.value.toString('utf8'));
+
+        if (isHistory && isHistory === true) {
+          jsonRes.TxId = res.value.tx_id;
+          jsonRes.Timestamp = res.value.timestamp;
+          jsonRes.IsDelete = res.value.is_delete.toString();
+          try {
+            jsonRes.Value = JSON.parse(res.value.value.toString('utf8'));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Value = res.value.value.toString('utf8');
+          }
+        } else {
+          jsonRes.Key = res.value.key;
+          try {
+            jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Record = res.value.value.toString('utf8');
+          }
+        }
+        arr.push(jsonRes);
+      }
+      if (res.done) {
+        await iterator.close();
+        console.info("arr: "+JSON.stringify(arr, null, 4));
+        console.info('============= END : Iterator to Array Result =============');
+        return arr;
+      }
+    }
+  }
 }
 
 
